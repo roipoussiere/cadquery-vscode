9941f8b92abcb6d1e9df026df630413e9fa8d201
Roipoussiere <roipoussiere@protonmail.com>
2022-03-28 23:20:49 +0200

    webview poc

--- extension.js
+++ extension.js
@@ -4,17 +4,36 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	// console.log('cadquery extension actived');
-
-	// commands are defined in package.json
 	let disposable = vscode.commands.registerCommand('cadquery.render', function () {
 		vscode.window.showInformationMessage('Rendering CadQuery model...');
+
+		const panel = vscode.window.createWebviewPanel(
+			'cadQuery',
+			'CadQuery view',
+			vscode.ViewColumn.One,
+			{}
+		);
+		panel.webview.html = getWebviewContent();
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 function deactivate() {}
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CadQuery view</title>
+  </head>
+  <body>
+    <h1>CadQuery view</h1>
+  </body>
+  </html>`;
+}
 
 module.exports = {
 	activate,
