2e0844f8c0278ee3bca6125a3723ea3f3a82c6df
Roipoussiere <roipoussiere@protonmail.com>
2022-03-29 03:25:48 +0200

    render the hexapod example in the cadquer view

--- extension.js
+++ extension.js
@@ -5,27 +5,36 @@ const fs = require('fs');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+function activate(ctx) {
 	let disposable = vscode.commands.registerCommand('cadquery.render', function () {
 		vscode.window.showInformationMessage('Rendering CadQuery model...');
 
 		const panel = vscode.window.createWebviewPanel(
 			'cadQuery', 'CadQuery view', vscode.ViewColumn.One, {
 				enableScripts: true,
-				localResourceRoots: [ vscode.Uri.file(path.join(context.extensionPath, 'cq-view')) ]
+				localResourceRoots: [ getResourcePath(ctx, '.') ]
 			}
 		);
 
-		const index_path = path.join(context.extensionPath, 'cq-view', 'index.html');
-		fs.readFile(index_path, 'utf-8', (err, data) => {
-			if(err) {
-				console.error(err)
-			}
-			panel.webview.html = data;
-		});
+		html = fs.readFileSync(getResourcePath(ctx, 'index.html').path).toString();
+
+		const cssPath = getResourcePath(ctx, 'cq-viewer.esm.css');
+		html = html.replace('{{cq-view-css}}', panel.webview.asWebviewUri(cssPath).toString());
+
+		const jsPath = getResourcePath(ctx, 'cq-viewer.esm.js');
+		html = html.replace('{{cq-view-js}}', panel.webview.asWebviewUri(jsPath).toString());
+
+		const examplePath = getResourcePath(ctx, 'hexapod.js');
+		html = html.replace('{{cq-view-example}}', panel.webview.asWebviewUri(examplePath).toString());
+
+		panel.webview.html = html;
 	});
 
-	context.subscriptions.push(disposable);
+	ctx.subscriptions.push(disposable);
+}
+
+function getResourcePath(context, resourcePath) {
+	return vscode.Uri.file(path.join(context.extensionPath, 'cq-viewer', resourcePath));
 }
 
 function deactivate() {}
