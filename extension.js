7ff462018cf7abcf5166560f1aef2c2194c3d806
Roipoussiere <roipoussiere@protonmail.com>
2022-03-29 18:20:57 +0200

    add update command

--- extension.js
+++ extension.js
@@ -2,6 +2,31 @@ const vscode = require('vscode');
 const path = require('path');
 const fs = require('fs');
 
+var sample_model = [
+	{
+		name: "Box1",
+		id: "/Box1",
+		parts: [
+			{
+				name: "Part_0",
+				id: "/Box1/Part_0",
+				type: "shapes",
+				shape: {
+					vertices: [ [-0.5, -0.5, -0.5], [-0.5, -0.5, 0.5], [-0.5, 0.5, -0.5], [-0.5, 0.5, 0.5], [0.5, -0.5, -0.5], [0.5, -0.5, 0.5], [0.5, 0.5, -0.5], [0.5, 0.5, 0.5], [-0.5, -0.5, -0.5], [0.5, -0.5, -0.5], [-0.5, -0.5, 0.5], [0.5, -0.5, 0.5], [-0.5, 0.5, -0.5], [0.5, 0.5, -0.5], [-0.5, 0.5, 0.5], [0.5, 0.5, 0.5], [-0.5, -0.5, -0.5], [-0.5, 0.5, -0.5], [0.5, -0.5, -0.5], [0.5, 0.5, -0.5], [-0.5, -0.5, 0.5], [-0.5, 0.5, 0.5], [0.5, -0.5, 0.5], [0.5, 0.5, 0.5] ],
+					triangles: [ 1, 2, 0, 1, 3, 2, 5, 4, 6, 5, 6, 7, 11, 8, 9, 11, 10, 8, 15, 13, 12, 15, 12, 14, 19, 16, 17, 19, 18, 16, 23, 21, 20, 23, 20, 22],
+					normals: [ [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [-1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [1, 0, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, -1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 1, 0], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, -1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1] ],
+					edges: [ [ [-0.5, -0.5, -0.5], [-0.5, -0.5, 0.5], ], [ [-0.5, -0.5, 0.5], [-0.5, 0.5, 0.5], ], [ [-0.5, 0.5, -0.5], [-0.5, 0.5, 0.5], ], [ [-0.5, -0.5, -0.5], [-0.5, 0.5, -0.5], ], [ [0.5, -0.5, -0.5], [0.5, -0.5, 0.5], ], [ [0.5, -0.5, 0.5], [0.5, 0.5, 0.5], ], [ [0.5, 0.5, -0.5], [0.5, 0.5, 0.5], ], [ [0.5, -0.5, -0.5], [0.5, 0.5, -0.5], ], [ [-0.5, -0.5, -0.5], [0.5, -0.5, -0.5], ], [ [-0.5, -0.5, 0.5], [0.5, -0.5, 0.5], ], [ [-0.5, 0.5, -0.5], [0.5, 0.5, -0.5], ], [ [-0.5, 0.5, 0.5], [0.5, 0.5, 0.5] ]],
+				},
+				color: "#e8b024",
+			}
+		],
+		loc: null,
+	},
+	{
+		"/Box1/Part_0": [1, 1],
+	}
+];
+
 /**
  * @param {vscode.ExtensionContext} context
  */
@@ -22,7 +47,6 @@ function activate(ctx) {
 			);
 
 			html = fs.readFileSync(getResource(ctx, 'index.html').path).toString();
-
 			html = html.replace('{{cq-view-css}}', getResourceUri(ctx, panel.webview, 'cq-viewer.esm.css'));
 			html = html.replace('{{cq-view-js}}', getResourceUri(ctx, panel.webview, 'cq-viewer.esm.js'));
 			html = html.replace('{{cq-view-example}}', getResourceUri(ctx, panel.webview, 'hexapod.js'));
@@ -33,13 +57,22 @@ function activate(ctx) {
 
 	function config() {
 		panel.webview.postMessage({
-			cadWidth: 400,
-			height: 300
+			options: {
+				cadWidth: 400,
+				height: 300
+			}
+		});
+	}
+
+	function update() {
+		panel.webview.postMessage({
+			model: sample_model
 		});
 	}
 
 	ctx.subscriptions.push(vscode.commands.registerCommand('cadquery.render', render));
 	ctx.subscriptions.push(vscode.commands.registerCommand('cadquery.config', config));
+	ctx.subscriptions.push(vscode.commands.registerCommand('cadquery.update', update));
 }
 
 function getResource(context, resourcePath) {
